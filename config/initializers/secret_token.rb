d046ada1e9890cf848ba393814e55b5808205033
Rajiev Timal <rtimal@gmail.com>
2012-08-09 20:31:08 -0400

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facepull::Application.config.secret_token = '85727ea50370fbcbce33e50e90c454318c0837ad8549112ca5f9d158e722b65b444379ad8283400d8d18a691aa6c689e2ebe84be4d2c2b39e10ac6a3815ab771'
