79fdd13b14197048774254c5e4a73b61f54f633a
Rajiev Timal <rtimal@gmail.com>
2012-08-09 20:51:19 -0400

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Facepull::Application.routes.draw do
+  get "site/gallery"
+
+  get "site/login"
+
+  get "site/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
