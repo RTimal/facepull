57de18af761c7c9b592f0685aef163223e3fd3a2
Rajiev Timal <rtimal@gmail.com>
2012-08-11 01:09:31 -0400

    cool

--- app/controllers/picture_controller.rb
+++ app/controllers/picture_controller.rb
@@ -11,7 +11,7 @@ class PictureController < ApplicationController
 	  
 	  File.open('app/assets/images/faceimage.gif', 'wb') do |fo|
 			fo.write open(params[:picurl]).read
-			send_data(fo , :filename => faceimage, :type=>'image/gif')
+			send_data(fo , :filename => 'faceimage.gif', :type=>'image/gif')
 	   end
 
 	  #render :text => location
